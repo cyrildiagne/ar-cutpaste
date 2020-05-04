52429c645a47d4ec5543f0c6607ade9f52995242
Cyril Diagne <diagne.cyril@gmail.com>
2020-05-04 19:01:09 +0200

    force unix path for the filename sent to photoshop
    fix #5

--- server/src/ps.py
+++ server/src/ps.py
@@ -10,7 +10,11 @@ DOC_WIDTH = 2121
 DOC_HEIGHT = 1280
 
 def paste(filename, name, x, y, password='123456'):
-
+    
+    # There seem to be a bug on Windows where the path must be using unix separators.
+    # https://github.com/cyrildiagne/ar-cutpaste/issues/5
+    filename = filename.replace('\\', '/')
+    
     with PhotoshopConnection(password=password) as conn:
         script = """
         function pasteImage(filename, layerName, x, y) {
